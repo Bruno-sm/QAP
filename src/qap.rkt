bb06068adae1b27bea9580e51cac890962ae8831
Bruno-sm <bru.1bruno@gmail.com>
2018-03-10 19:39:33 +0100

    Implements results with arithmetic mean

--- src/qap.rkt
+++ src/qap.rkt
@@ -6,77 +6,99 @@
 
 
 (struct cli-options (algorithm
+                     executions-number
                      max-iterations
                      output-mode)
                     #:mutable)
 
 (define (main)
-  (define options (cli-options 'greedy 50000 'human-readable))
+  (define options (cli-options 'greedy 1 50000 'human-readable))
   (define files empty)
   (command-line
     #:program "qap"
     #:once-each
-    [("-m" "--max-iterations") mi
-             "Maximum number of iterations"
-             (set-cli-options-max-iterations! options (string->number mi))]
+    [("-m" "--max-iterations")
+     mi
+     "Maximum number of iterations inside an algorithm"
+     (set-cli-options-max-iterations! options (string->number mi))]
+    [("-r" "--repetitions")
+     exec-number
+     "Number of executions for each case. The result is the mean of all executions"
+     (set-cli-options-executions-number! options (string->number exec-number))]
     #:once-any
-    [("--csv-output") "Displays the output in csv format"
-             (set-cli-options-output-mode! options 'csv)]
+    [("--csv-output")
+     "Displays the output in csv format"
+     (set-cli-options-output-mode! options 'csv)]
     #:once-any
-    [("--greedy") "Uses the greedy algorithm" 
-           (set-cli-options-algorithm! options 'greedy)]
-    [("--local-search-bf") "Uses the local search algorithm with best-first selection"
-           (set-cli-options-algorithm! options 'local-bf)]
-    [("--local-search-bn") "Uses the local search algorithm with best neighbor selection"
-           (set-cli-options-algorithm! options 'local-bn)]
+    [("--greedy")
+     "Uses the greedy algorithm"
+     (set-cli-options-algorithm! options 'greedy)]
+    [("--local-search-bf")
+     "Uses the local search algorithm with best-first selection"
+     (set-cli-options-algorithm! options 'local-bf)]
+    [("--local-search-bn")
+     "Uses the local search algorithm with best neighbor selection"
+     (set-cli-options-algorithm! options 'local-bn)]
     #:args (file . more-files) (set! files (cons file more-files)))
   (set! files (map (lambda (f) (build-path (current-directory) f)) files))  
   (cond [(symbol=? (cli-options-output-mode options) 'csv)
-         (displayln "Case,value,opt_value,cpu_ms,real_ms,gc_ms")])
-  (cond
-    [(symbol=? (cli-options-algorithm options) 'greedy)
-     (for-each (lambda (file) (print-solution greedy file options)) files)]
-    [(symbol=? (cli-options-algorithm options) 'local-bf)
-     (for-each (lambda (file)
-                 (print-solution local-search
-                                 file
-                                 options
-                                 `(,(cli-options-max-iterations options)
-                                   ,best-first-dlb-selection)))
-               files)]
-    [(symbol=? (cli-options-algorithm options) 'local-bn)
-     (for-each (lambda (file)
-                 (print-solution local-search
-                                 file
-                                 options
-                                 `(,(cli-options-max-iterations options)
-                                   ,best-neighbor-selection)))
-               files)]))
+         (displayln "Case,repetitions,best_value,values_mean,opt_value,cpu_ms,real_ms,gc_ms")])
+  (match-define-values (algorithm alg-args)
+    (let ([alg (cli-options-algorithm options)])
+      (cond
+        [(symbol=? alg 'greedy) (values greedy empty)]
+        [(symbol=? alg 'local-bf)
+         (values local-search `(,(cli-options-max-iterations options)
+                                ,best-first-dlb-selection))]
+        [(symbol=? alg 'local-bn)
+         (values local-search `(,(cli-options-max-iterations options)
+                                ,best-neighbor-selection))])))
+  (for-each (lambda (file) (print-solution algorithm file options alg-args)) files))
 
 (define (print-solution algorithm file cli-options [args empty])
   (match-define-values (size fm dm) (read-dat file))
-  (match-define-values (ret cpu-ms real-ms gc-ms)
-                       (time-apply algorithm (append (list size fm dm) args)))
-  (define solution (first ret))
+  (define solution empty)
+  (define solution-goodness -1)
+  (define goodness-mean 0.0)
+  (define cpu-ms 0.0)
+  (define real-ms 0.0)
+  (define gc-ms 0.0)
+  (for ([i (cli-options-executions-number cli-options)])
+    (match-define-values (ret cpu-ms* real-ms* gc-ms*)
+                         (time-apply algorithm (append (list size fm dm) args)))
+    (set! cpu-ms (+ cpu-ms cpu-ms*))
+    (set! real-ms (+ real-ms real-ms*))
+    (set! gc-ms (+ gc-ms gc-ms*))
+    (let* ([s (first ret)] [sg (qap-goodness s fm dm)])
+      (set! goodness-mean (+ goodness-mean sg))
+      (cond [(or (= -1 solution-goodness) (< sg solution-goodness))
+             (set! solution s)
+             (set! solution-goodness sg)])))
+  (set! cpu-ms (/ cpu-ms (cli-options-executions-number cli-options)))
+  (set! real-ms (/ real-ms (cli-options-executions-number cli-options)))
+  (set! gc-ms (/ gc-ms (cli-options-executions-number cli-options)))
+  (set! goodness-mean (/ goodness-mean (cli-options-executions-number cli-options)))
   (match-define-values (_ opt-goodness opt-solution)
                        (read-sln (path-replace-extension file #".sln")))
   (cond
     [(symbol=? (cli-options-output-mode cli-options) 'csv)
-     (displayln (format "~a,~a,~a,~a,~a,~a"
+     (displayln (format "~a,~a,~a,~a,~a,~a,~a,~a"
                         (file-name-from-path file)
-                        (qap-goodness solution fm dm) opt-goodness
+                        (cli-options-executions-number cli-options)
+                        solution-goodness goodness-mean opt-goodness
                         cpu-ms real-ms gc-ms))]
     [else
      (displayln (format "file: ~a" (path->string (file-name-from-path file))))
      (displayln (format "size: ~a" size))
-     (displayln (format "solution: ~a" (map (lambda (x) (add1 x)) solution)))
-     (displayln (format "goodness: ~a" (qap-goodness solution fm dm)))
-     (displayln (format "best solution: ~a" opt-solution))
-     (displayln (format "best goodness: ~a"
-                        (qap-goodness (map (lambda (x) (sub1 x)) opt-solution) fm dm)))
-     (displayln (format "cpu mlliseconds: ~a" cpu-ms))
-     (displayln (format "real mlliseconds: ~a" real-ms))
-     (displayln (format "garbage collection mlliseconds: ~a" gc-ms))
+     (displayln (format "repetitions: ~a" (cli-options-executions-number cli-options)))
+     (displayln (format "best solution: ~a" (map (lambda (x) (add1 x)) solution)))
+     (displayln (format "best goodness: ~a" solution-goodness))
+     (displayln (format "goodness mean: ~a" goodness-mean))
+     (displayln (format "optimal solution: ~a" opt-solution))
+     (displayln (format "optimal goodness: ~a" opt-goodness))
+     (displayln (format "cpu mlliseconds mean: ~a" cpu-ms))
+     (displayln (format "real mlliseconds mean: ~a" real-ms))
+     (displayln (format "garbage collection mlliseconds mean: ~a" gc-ms))
      (displayln "")]))
 
 
