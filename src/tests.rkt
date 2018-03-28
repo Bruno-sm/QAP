87d82bd5405ed5127fe3ab1ff5f826d73d968ce4
Bruno-sm <bru.1bruno@gmail.com>
2018-03-28 22:49:23 +0200

    Adds tests for qap-goodness

--- src/tests.rkt
+++ src/tests.rkt
@@ -7,6 +7,22 @@
          "data-reader.rkt"
          "tabu-search.rkt")
 
+(define qap-goodness-tests
+  (test-suite
+   "Tests for qap-goodness function"
+   (let ([dat-files '("data/chr22a.dat" "data/wil50.dat" "data/wil100.dat"
+                      "data/lipa60b.dat" "data/tai150b.dat" "data/sko81.dat"
+                      "data/had20.dat" "data/esc128.dat" "data/tho40.dat")]
+         [sln-files '("data/chr22a.sln" "data/wil50.sln" "data/wil100.sln"
+                      "data/lipa60b.sln" "data/tai150b.sln" "data/sko81.sln"
+                      "data/had20.sln" "data/esc128.sln" "data/tho40.sln")])
+     (for ([df dat-files] [sf sln-files])
+       (match-let-values ([(size fm dm) (read-dat df)]
+                           [(_ goodness solution) (read-sln sf)])
+         (check-equal? (qap-goodness (map (lambda (x) (sub1 x)) solution) fm dm)
+                       goodness 
+                       df))))))
+
 (define qap-goodness-permutation-diff-tests
   (test-suite
    "Tests for qap-goodness-permutation-diff function"
@@ -39,5 +55,6 @@
                        (format "sol1: ~a, sol2: ~a, sol3: ~a, tr1: ~a, tr2: ~a"
                                sol1 sol2 sol3 trans1 trans2)))))))
 
+(run-tests qap-goodness-tests)
 (run-tests qap-goodness-permutation-diff-tests)
-(run-tests tabu-goodness-diff-tests)
+;(run-tests tabu-goodness-diff-tests)
